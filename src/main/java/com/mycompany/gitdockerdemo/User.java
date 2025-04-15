80440884b9b004a6c96cc15759a7a604c3055146
tranthanhhieu4 <hieuco410@gmail.com>
2025-04-15 10:04:32 +0700

    Add User class

---
+++ src/main/java/com/mycompany/gitdockerdemo/User.java
@@ -0,0 +1,21 @@
+
+
+package com.mycompany.gitdockerdemo;
+
+
+public class User {
+private String userId;
+    private String email;
+
+    public User(String userId, String email) {
+        this.userId = userId;
+        this.email = email;
+    }
+
+    public String getEmail() { return email; }
+
+    public void setEmail(String email) { this.email = email; }
+
+    public void updateEmail(String newEmail) { this.email = newEmail; }
+    
+}
